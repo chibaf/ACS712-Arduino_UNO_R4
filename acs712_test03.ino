336af04ee654bc0b5c686f13ae3f30617f2f02d6
chibaf <chibaf@mac.com>
2025-01-26 09:36:32 +0900

    Add files via upload

---
+++ acs712_test03.ino
@@ -0,0 +1,26 @@
+//CAS712
+//使う電流センサのアンペア数
+double Ampere = 5;
+//現在の電流値(mA)．
+double current;
+//オフセット値(V)．デフォだと0Vの時センサ値は512(レンジを5Vにすると2.5V)なので ．
+double offset = 2.505;
+//レンジ調整値．0Vに調整した後，電源等でこの電流計の値が合っているかをチェックして，ズレていれば調整する．
+double range = 0.95;
+//forを回す回数．大きくすると処理が遅くなるが，電流値は安定する．10000times/secがanalogReadの限界な事に注意．
+int times = 100;
+
+void setup() {
+  Serial.begin(9600);
+  analogReadResolution(14); 
+}
+
+void loop() {
+  current = 0;
+  for(int i = 0; i < times; i++){
+    //1.5~3.5Vレンジなので2.5Vオフセットすると-1~1V．これを-5~5Vにする為5をかけている．
+    current += ((double)(analogRead(A0)) / 1023 * Ampere - offset) / range * 5 * 1000 / (double)(times);
+  }
+  Serial.print("Current(mA) = ");
+  Serial.println(round(current));
+}
